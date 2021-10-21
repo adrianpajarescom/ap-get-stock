dbecb802942ce67be423594aa82d3977081777a5
adrianpajares.com <92946266+adrianpajarescom@users.noreply.github.com>
2021-10-21 20:08:10 +0200

    Update ap-get-stock.php

--- ap-get-stock.php
+++ ap-get-stock.php
@@ -10,3 +10,10 @@
  * GitHub Plugin URI: https://github.com/adrianpajarescom/ap-get-stock
  * GitHub Languages:  https://github.com/adrianpajarescom/ap-get-stock
  */
+
+
+
+function shortcode_ap_get_stock() {
+return '<p>¡Gracias por leer mi blog!</p>';
+}
+add_shortcode('ap_get_stock', 'shortcode_ap_get_stock');
