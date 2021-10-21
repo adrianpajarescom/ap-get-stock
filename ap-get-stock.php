fcdd5ebd9052f28f00a9361402940ac0f87f990f
adrianpajares.com <92946266+adrianpajarescom@users.noreply.github.com>
2021-10-21 20:01:44 +0200

    Create ap-get-stock.php

---
+++ ap-get-stock.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name:       Git Updater
+ * Plugin URI:        https://github.com/afragen/git-updater
+ * Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
+ * Version:           10.0.0
+ * Author:            Andy Fragen
+ * License:           MIT
+  * Domain Path:       /languages
+ * Text Domain:       git-updater
+ * GitHub Plugin URI: https://github.com/afragen/git-updater
+ * GitHub Languages:  https://github.com/afragen/git-updater-translations
+ */
