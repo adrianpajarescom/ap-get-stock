e871ea255d481bf30e2f193bfa17645a0445fb7a
adrianpajares.com <92946266+adrianpajarescom@users.noreply.github.com>
2021-10-21 20:03:43 +0200

    Update ap-get-stock.php

--- ap-get-stock.php
+++ ap-get-stock.php
@@ -1,12 +1,12 @@
 /**
- * Plugin Name:       Git Updater
- * Plugin URI:        https://github.com/afragen/git-updater
- * Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
- * Version:           10.0.0
- * Author:            Andy Fragen
+ * Plugin Name:       adrianpajares.com
+ * Plugin URI:        https://github.com/adrianpajarescom/ap-get-stock
+ * Description:       Plugin to get the product stock.
+ * Version:           1.0
+ * Author:            adrianpajares.com
  * License:           MIT
   * Domain Path:       /languages
- * Text Domain:       git-updater
- * GitHub Plugin URI: https://github.com/afragen/git-updater
- * GitHub Languages:  https://github.com/afragen/git-updater-translations
+ * Text Domain:       ap-get-stock
+ * GitHub Plugin URI: https://github.com/adrianpajarescom/ap-get-stock
+ * GitHub Languages:  https://github.com/adrianpajarescom/ap-get-stock
  */
