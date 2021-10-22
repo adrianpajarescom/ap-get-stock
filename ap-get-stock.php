e703c8e57ca1f936c921c672728f8d1a38b4c4e0
adrianpajares.com <desarrollo@adrianpajares.com>
2021-10-22 11:14:22 +0200

    Update ap-get-stock.php

--- ap-get-stock.php
+++ ap-get-stock.php
@@ -2,22 +2,17 @@
 
 /**
  * Plugin Name:       adrianpajares.com - Get Stock
- * Plugin URI:        https://github.com/adrianpajarescom/ap-get-stock
+ * Plugin URI:        https://adrianpajares.com/
  * Description:       Plugin to get the product stock. Shotcode: [ap_get_stock]
  * Version:           1.0
  * Author:            adrianpajares.com
  * License:           MIT
- * Domain Path:       /languages
- * Text Domain:       ap-get-stock
  */
-
-
 
 function shortcode_ap_get_stock() {
     
     global $product; 
     $stock  = $product->get_stock_quantity();
-    $id = get_the_ID();
 
     return $stock;
 }
